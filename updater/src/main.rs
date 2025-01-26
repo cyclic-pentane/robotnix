479a31c74d89de9ffeb4800f42ab33f9864005f9
cyclopentane <cyclopentane@aidoskyneen.eu>
2025-01-26 18:47:52 +0100

    Incremental fetching in updater program

--- updater/src/main.rs
+++ updater/src/main.rs
@@ -56,7 +56,6 @@ struct FetchgitArgs {
 
 #[derive(Debug, Serialize, Deserialize)]
 struct Device {
-    branch: String,
     deps: HashMap<String, FetchgitArgs>,
 }
 
@@ -66,6 +65,19 @@ fn get_device_repo(vendor: &str, device: &str) -> Vec<String> {
 
 fn get_repo_url(repo: &Vec<String>) -> String {
     format!("https://github.com/LineageOS/{}", repo.join("_"))
+}
+
+fn ls_remote(repo: &Vec<String>, branch: &str) -> String {
+    let url = get_repo_url(repo);
+    println!("ls-remote'ing {}", &url);
+    let output = Command::new("git")
+        .arg("ls-remote")
+        .arg(&url)
+        .arg(format!("refs/heads/{branch}"))
+        .output()
+        .unwrap()
+        .stdout;
+    std::str::from_utf8(output.split(|x| x == &b'\t').nth(0).unwrap()).unwrap().to_string()
 }
 
 fn nix_prefetch_git_repo(repo: &Vec<String>) -> NixPrefetchGitOutput {
@@ -83,32 +95,43 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let device_metadata_path = &args[1];
 
-    let file = File::open(device_metadata_path).unwrap();
-    let reader = BufReader::new(file);
+    let reader = BufReader::new(File::open(device_metadata_path).unwrap());
     let devices: HashMap<String, DeviceMetadata> = serde_json::from_reader(reader).unwrap();
 
-    let mut device_dir_entries: HashMap<String, Device> = HashMap::new();
+    // Read pre-existing device_dirs.json for incremental updates.
+    let reader = BufReader::new(File::open(&args[2]).unwrap());
+    let mut device_dir_entries: HashMap<String, Device> = serde_json::from_reader(reader).unwrap();
     for (device_name, device_metadata) in devices.iter() {
-        let device_repo = get_device_repo(&device_metadata.vendor, &device_name);
-
-        let mut deps: HashMap<String, FetchgitArgs> = HashMap::new();
-        for dep in device_metadata.deps.iter() {
-            let output = nix_prefetch_git_repo(dep);
-            deps.insert(dep[1..].join("/"), FetchgitArgs {
-                url: output.url,
-                rev: output.rev,
-                hash: output.hash,
-                fetch_lfs: output.fetch_lfs,
-                fetch_submodules: output.fetch_submodules,
-                deep_clone: output.deep_clone,
-                leave_dot_git: output.leave_dot_git,
+        if !device_dir_entries.contains_key(device_name) {
+            device_dir_entries.insert(device_name.clone(), Device {
+                deps: HashMap::new(),
             });
         }
+        let mut device = device_dir_entries.get_mut(device_name).unwrap();
+        let device_repo = get_device_repo(&device_metadata.vendor, &device_name);
 
-        device_dir_entries.insert(device_name.clone(), Device {
-            branch: device_metadata.branch.clone(),
-            deps: deps
-        });
+        for dep in device_metadata.deps.iter() {
+            let path = dep[1..].join("/");
+            let is_up_to_date = if let Some(args) = device.deps.get(&path) {
+                let current_rev = ls_remote(dep, &device_metadata.branch);
+                current_rev == args.rev
+            } else {
+                false
+            };
+
+            if !is_up_to_date {
+                let output = nix_prefetch_git_repo(dep);
+                device.deps.insert(path, FetchgitArgs {
+                    url: output.url,
+                    rev: output.rev,
+                    hash: output.hash,
+                    fetch_lfs: output.fetch_lfs,
+                    fetch_submodules: output.fetch_submodules,
+                    deep_clone: output.deep_clone,
+                    leave_dot_git: output.leave_dot_git,
+                });
+            }
+        }
     }
 
     let device_dirs_json = serde_json::to_string(&device_dir_entries).unwrap();
