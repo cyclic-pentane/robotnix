f5040d3e87ef98f19e2f2e01f63367dc9787a8b6
cyclopentane <cyclopentane@aidoskyneen.eu>
2025-02-20 17:06:10 +0100

    Further work on updater

--- updater/src/main.rs
+++ updater/src/main.rs
@@ -59,16 +59,56 @@ struct Device {
     deps: HashMap<String, FetchgitArgs>,
 }
 
-fn get_device_repo(vendor: &str, device: &str) -> Vec<String> {
-    vec!["android", "device", vendor, device].iter().map(|x| x.to_string()).collect()
+enum Remote {
+    LineageOS,
+    TheMuppetsGitHub,
+    TheMuppetsGitLab
 }
 
-fn get_repo_url(repo: &Vec<String>) -> String {
-    format!("https://github.com/LineageOS/{}", repo.join("_"))
+struct Repository {
+    remote: Remote,
+    path: Vec<String>
 }
 
-fn ls_remote(repo: &Vec<String>, branch: &str) -> String {
-    let url = get_repo_url(repo);
+impl Remote {
+    fn base_url(&self) -> String {
+        match self {
+            Repository::LineageOS => "https://github.com/LineageOS/",
+            Repository::TheMuppetsGitHub => "https://github.com/TheMuppets",
+            Repository::TheMuppetsGitLab => "https://gitlab.com/TheMuppets"
+        }.to_string()
+    }
+}
+
+impl Repository {
+    fn new_device_repo(vendor: &str, device: &str) -> Repository {
+        Repository {
+            remote: Remote::LineageOS,
+            path: vec![
+                "android".to_string(),
+                "device".to_string(),
+                vendor.clone(),
+                device.clone()
+            ]
+        }
+    }
+
+    fn url(&self) -> String {
+        format!("{}{}", &self.repo.base_url(), &self.path.join("_")
+    }
+
+    fn source_tree_path(&self) -> String {
+        match self.path.get(0) {
+            Some("android") => &self.path[1..],
+            Some("proprietary") => &self.path[1..],
+            Some(xs) => xs,
+            None => &vec![]
+        }.join("/")
+    }
+}
+
+fn ls_remote(repo: &Repository, branch: &str) -> String {
+    let url = repo.url();
     println!("ls-remote'ing {}", &url);
     let output = Command::new("git")
         .arg("ls-remote")
@@ -91,6 +131,8 @@ fn nix_prefetch_git_repo(repo: &Vec<String>) -> NixPrefetchGitOutput {
     serde_json::from_slice(&output.stdout).unwrap()
 }
 
+fn get_corresponding_vendor_repos(repo: &Vec<String>) -> Nix
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     let device_metadata_path = &args[1];
@@ -99,8 +141,8 @@ fn main() {
     let devices: HashMap<String, DeviceMetadata> = serde_json::from_reader(reader).unwrap();
 
     // Read pre-existing device_dirs.json for incremental updates.
-    let reader = BufReader::new(File::open(&args[2]).unwrap());
-    let mut device_dir_entries: HashMap<String, Device> = serde_json::from_reader(reader).unwrap();
+    let reader = File::open(&args[2]).map(|f| BufReader::new(f));
+    let mut device_dir_entries: HashMap<String, Device> = reader.map(|r| serde_json::from_reader(r)).unwrap_or(HashMap::new());
     for (device_name, device_metadata) in devices.iter() {
         if !device_dir_entries.contains_key(device_name) {
             device_dir_entries.insert(device_name.clone(), Device {
@@ -108,7 +150,6 @@ fn main() {
             });
         }
         let mut device = device_dir_entries.get_mut(device_name).unwrap();
-        let device_repo = get_device_repo(&device_metadata.vendor, &device_name);
 
         for dep in device_metadata.deps.iter() {
             let path = dep[1..].join("/");
